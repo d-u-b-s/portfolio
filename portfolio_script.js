b35deaa12bcbcb5f208c622c9af2561e6cc6cae6
d-u-b-s <119791407+d-u-b-s@users.noreply.github.com>
2023-04-05 18:28:04 +0200

    Version 1.0

---
+++ portfolio_script.js
@@ -0,0 +1,16 @@
+const images = document.querySelectorAll(".clickable");  
+const modal = document.querySelector(".modal");
+const modalImg = document.querySelector(".modalImg");
+const close = document.querySelector(".close");
+
+images.forEach((image) => {
+    image.addEventListener("click", () => {
+        modalImg.src = image.src;
+    
+        modal.classList.add("appear");
+
+        close.addEventListener("click", () => {
+            modal.classList.remove("appear");
+        })
+    });
+});
